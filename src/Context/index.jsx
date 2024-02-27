a7ff56bd5c5da42d5252832fa8ef919d3dfabe2d
Brendon Victor <devbrendonvs@gmail.com>
2024-02-27 09:52:44 -0300

    Context - Estados criados

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,7 +3,29 @@ import { createContext, useState } from "react";
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-  
+  const [total, setTotal] = useState(0);
+  const [entrada, setEntrada] = useState(0);
+  const [saida, setSaida] = useState(0);
+  const [listaGastos, setListaGastos] = useState([]);
+  const [tipoTransacao, setTipoTransacao] = useState(true);
 
-  return <Context.Provider value={{}}>{children}</Context.Provider>;
+
+  return (
+    <Context.Provider
+      value={{
+        total,
+        entrada,
+        saida,
+        listaGastos,
+        tipoTransacao,
+        setTipoTransacao,
+        setTotal,
+        setEntrada,
+        setSaida,
+        setListaGastos,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
