c0bb246cdfde6c1552ff8f8ec8aeaec7d5b036ae
Brendon Victor <devbrendonvs@gmail.com>
2024-02-27 10:25:36 -0300

    Components/NovaTransacao/OpcoesDeTransacao configurado

--- src/Components/NovaTransacao/OpcoesDeTransacao.jsx
+++ src/Components/NovaTransacao/OpcoesDeTransacao.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
-export const OpcoesDeTransacao = () => {
+export default function OpcaoTransacao(prop) {
   return (
-    <div>OpcoesDeTransacao</div>
-  )
+    <FormControl
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        width: "100%",
+        padding: "5px 0",
+      }}
+    >
+      <FormLabel id="demo-radio-buttons-group-label">
+        Tipo de transação:
+      </FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="entrada"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel
+          value="entrada"
+          control={<Radio onChange={() => prop.func.setTipoTransacao(true)} />}
+          label="Entrada"
+        />
+        <FormControlLabel
+          value="saida"
+          control={<Radio onChange={() => prop.func.setTipoTransacao(false)} />}
+          label="Saída"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
 }
