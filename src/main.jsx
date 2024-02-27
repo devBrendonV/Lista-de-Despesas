f316ca28ff4927dabc20571bc0d0e33dff527c8c
Brendon Victor <devbrendonvs@gmail.com>
2024-02-27 09:21:37 -0300

    App e main configurados

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ContextProvider } from "./Context";
+import { CssBaseline } from "@mui/material";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ContextProvider>
+      <App />
+      <CssBaseline/>
+    </ContextProvider>
+  </React.StrictMode>
+);
